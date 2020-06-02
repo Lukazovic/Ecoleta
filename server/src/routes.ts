31d28df7d214c7b1d2cf0fb2dab04c294a7114a8
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-06-02 13:21:19 -0300

    Add controllers to the backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import knex from './database/connection';
+
+import PointsController from './app/controllers/PointsController';
+import ItemsController from './app/controllers/ItemsController';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello World',
-  });
-});
+routes.get('/items', ItemsController.index);
+
+routes.get('/points', PointsController.index);
+routes.get('/points/:id', PointsController.show);
+routes.post('/points', PointsController.create);
 
 export default routes;
